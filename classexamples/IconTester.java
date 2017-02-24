ccf675b96aedebd810419ff8a40e8dd4c62b406c
lornaxiao <lornaxiao@rth-236-2q100a.ds.slu.edu>
2017-02-24 11:43:34 -0600

    new example

---
+++ classexamples/IconTester.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class IconTester {
+	public static void main(String args[])
+	{
+		JOptionPane.showMessageDialog(null,"Hello World!","Message",JOptionPane.INFORMATION_MESSAGE,
+				new ImageIcon("globe.png"));
+		System.exit(0);
+	}
+}
